ea34c550d174d53fb230a64756aa04637c5284f4
Sambrewer <sam.brewer13@yahoo.com>
2020-05-23 19:58:18 -0600

    tuning movement speed added vertical movement

--- Assets/Standard Assets/CrossPlatformInput/Scripts/Player.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Player.cs
@@ -7,8 +7,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 15f;
+    [Tooltip("In m")] [SerializeField] float yRange = 9f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +20,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
-        
-        float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
