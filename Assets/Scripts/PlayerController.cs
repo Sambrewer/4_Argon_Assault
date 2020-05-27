6c197d844b6d9e2a1119a706dd26a7cf494c8b81
Sambrewer <sam.brewer13@yahoo.com>
2020-05-26 23:19:57 -0600

    added player death

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 15f;
+    [Tooltip("In m")] [SerializeField] float yRange = 10f;
+
+    [Header("Screen Coupled")]
+    [SerializeField] float positionPitchFactor = -1.5f;
+    [SerializeField] float positionYawFactor = 1f;
+
+    [Header("Control Coupled")]
+    [SerializeField] float controlRollFactor = -20f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+
+    }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
