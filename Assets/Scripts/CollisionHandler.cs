7aec18a77d03a48493aa203819d03530b4c626c7
Sambrewer <sam.brewer13@yahoo.com>
2020-05-28 23:53:29 -0600

    death consequences

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")]  [SerializeField] GameObject deathFX;
+
     void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        ReloadScene();
+    }
+
+    private void ReloadScene()
+    {
+        throw new NotImplementedException();
     }
 
     private void StartDeathSequence()
     {
-        SendMessage("OnPlayerDeath");
+        SceneManager.LoadScene(1);
     }
 }
